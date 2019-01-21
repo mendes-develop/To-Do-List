a47082327a21a50edcf96b597c9e515d9caf407f
Alex Mendes <mendes.develop@gmail.com>
2019-01-20 23:14:29 -0500

    AddButtonPressed: UIAlert, UIAlertAction, AddTextField, Tableview.reloadData

--- Test/ToDoListViewController.swift
+++ Test/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-        let itemArray = ["Buy milk","Pick up kids","Pay rent"]
+    var itemArray = ["Buy milk","Pick up kids","Pay rent"]
     
     
     
@@ -49,6 +49,43 @@ class ToDoListViewController: UITableViewController {
         
         
     }
+    
+    // MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new task", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //what will happen once the user hits "add item"
+            
+            self.itemArray.append(textField.text!)
+            print(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Creat new item"
+            textField = alertTextField
+            
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
     
 }
 
